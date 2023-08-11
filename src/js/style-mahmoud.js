d4558e3960e1988fe8b43a333e66fc3f92c39b6b
mahmoud majid <https://github.com/mahmoud11mg/prosthetics>
2023-08-10 22:19:53 -0700

    edit  new

--- src/js/style-mahmoud.js
+++ src/js/style-mahmoud.js
@@ -46,12 +46,12 @@ function scrollFunction() {
   } else {
       scrollToTopBtn.style.display = "none";
   }
-}
+};
 
 function scrollToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
 
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 scrollToTopBtn.addEventListener("click", scrollToTop);
